7726cd2cfa3d18b627e0d72bfd40cf5c8fe2d8f5
friedrich <friedrich@pop-os.localdomain>
2022-05-11 19:28:03 +0200

    add plugin.lua with packer and monokai theme

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("plugins")
+
+-- theme
+require('monokai').setup {}
---
+++ lua/plugins.lua
@@ -0,0 +1,10 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- themes
+  use 'tanvirtin/monokai.nvim'
+
+
+
+end)
