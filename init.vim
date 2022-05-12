d87bff22ab97409a5483463ec616e1c07af87dd4
Friedrich Wilken <jens.wilken@sap.com>
2022-05-12 19:52:49 +0200

    add plugin markdown preview

--- init.vim
+++ init.vim
@@ -11,3 +11,9 @@ nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+""markdown preview
+" example
+nmap <C-s> <Plug>MarkdownPreview
+nmap <M-s> <Plug>MarkdownPreviewStop
+nmap <C-p> <Plug>MarkdownPreviewToggle
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,32 +1,35 @@
 return require('packer').startup(function()
-  -- Packer can manage itself
+  --> Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- themes
+  --> themes
   use 'tanvirtin/monokai.nvim'
 
-  -- LSP
+  --> LSP
   use 'neovim/nvim-lspconfig' 
   
-  -- autocomplete
+  --> autocomplete
   use 'hrsh7th/nvim-cmp'		-- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp'		-- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip'	-- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip'		-- Snippets plugin
 
-  --telescope
+  --> telescope
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  --lualine
+  --> lualine
   use {
   'nvim-lualine/lualine.nvim',
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   
-  --treesitter
+  --> treesitter
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  --> markdown preview
+  use 'iamcco/markdown-preview.nvim' 
 end)
 
