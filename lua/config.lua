276e5d0e26bdc525e0f19fa4b1f6344f4127ce16
Friedrich Wilken <jens.wilken@sap.com>
2022-07-28 15:11:59 +0200

    add keymap for "go to type def" and "rename symbol"

--- lua/config.lua
+++ lua/config.lua
@@ -41,6 +41,9 @@ for _, lsp in ipairs(servers) do
     on_attach = function()
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer= 0})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer= 0})
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer= 0})
+      
+      vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, {buffer= 0})
     end,
     capabilities = capabilities,
   }
